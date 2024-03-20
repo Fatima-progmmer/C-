95737682b6ad1862407a205713fb8f3b5888916b
Kashmiri Queen <149133736+Fatima-progmmer@users.noreply.github.com>
2024-03-19 20:41:53 -0700

    Add files via upload

---
+++ 3.cxx
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r ;
+	float Area,n=3.147;
+	cout << "Enter a radius of circle=";
+	cin >> r;
+	Area =n*r*2;
+	cout << "Area of circle =" << Area;
+	return 0;
+}
